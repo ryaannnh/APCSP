5a498964ca9a2699744235c28281c26c10b29987
ryaannnh <112128343+ryaannnh@users.noreply.github.com>
2023-02-09 16:59:07 -0800

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,13 +1,22 @@
+# Objective: to recreate Mario in Python.
+# Pseudocode in remark after each line
+# Ryan Ho 2/7/23
+
 import cs50
 
-height = int(input("Height: ")) #input is always a string, so need to do int() to typecast
-x = 1
-for i in range(height+1):
-    for p in range(height-i):
-        print(" ", end = '')
-    for j in range(i):
+flag= True #flag for determining if height qualifies
+while flag: #repeat until flag is switched to false
+    height = int(input("Height: ")) #input is always a string, so need to do int() to typecast
+    x = 1
+    if height < 9 and height > 0:
+        flag = False #switch flag to false if requirements fulfilled
+
+for i in range(height): #for "height" number of rows
+    for p in range(height-i-1): #For loop to print spaces
+        print("  ", end = '') #Print two spaces without new line
+    for j in range(i+1): #Print hashtags
         print("#", end = '')
-    print(" ", end='')
-    for k in range(i):
+    print(" ", end='') #Print space between hashtag
+    for k in range(i+1): #print remaining hashtag
         print("#", end = '')
-    print()
+    print() #print newline
